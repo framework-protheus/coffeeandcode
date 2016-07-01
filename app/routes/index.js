b8964b90798158fbd284c6cdaf344bb8c4f69e87
Arthur Fucher <arthurfucher@gmail.com>
2016-07-01 14:03:59 -0300

    changed redirect to 401 when unauthorized

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,10 +6,12 @@ module.exports = function(app, passport) {
 
 // route middleware to make sure a user is logged in
 function checkLogin(req, res, next) {
-    // if user is authenticated in the session, carry on 
+    // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
 
     // if they aren't redirect them to the home page
-    res.redirect('/');
+    //res.redirect('/');
+    res.status(401);
+    res.end("User don't has access");
 }
