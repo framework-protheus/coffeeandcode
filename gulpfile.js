36e8057245feabc6e56bd0f7a7633069c4cda6a8
Arthur Fücher <arthurfucher@gmail.com>
2016-06-30 22:04:53 -0300

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('istanbul', function (cb) {
+    gulp.src('./app/**/*.js')
+        .pipe(plugins.istanbul({
+            includeUntested: true
+        })) // Covering files
+        .pipe(plugins.istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(['./test/**/*.js'])
+                .pipe(plugins.mocha())
+                .pipe(plugins.istanbul.writeReports({
+                    reporters: ['text', 'text-summary']
+                }))
+                .on('end', cb);
+        });
+});
